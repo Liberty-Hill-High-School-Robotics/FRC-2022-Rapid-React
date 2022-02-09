33ca8c92b408271d29e597d8ab943d6afa65dd8a
Katie <KatiePilot928@gmail.com>
2022-02-09 17:19:56 -0600

    Created turret and hood commands and put motors in them. I also moved the "if(m_intake.isBallInIntake()) from IntakeInCommand to IntakeToFull

--- 2022RapidReactCode/src/main/java/frc/robot/commands/IntakeInCommand.java
+++ 2022RapidReactCode/src/main/java/frc/robot/commands/IntakeInCommand.java
@@ -68,14 +68,7 @@ public class IntakeInCommand extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-                if(m_intake.isBallInIntake())   {
-            BeltUpCommand m_BeltUpCommand=new BeltUpCommand(RobotContainer.getInstance().m_belt);
-            m_BeltUpCommand.schedule();
-            return true;
-        }
-        else {
-            return false;
-        }
+       return false;
     }
 
     @Override
