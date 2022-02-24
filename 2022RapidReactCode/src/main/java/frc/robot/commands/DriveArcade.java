13bfc480132599b5c49ca0b4b1f20a4708009392
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2022-02-24 15:12:53 -0600

    Get X
    Change drive controls to get X to turn instead of Z

--- 2022RapidReactCode/src/main/java/frc/robot/commands/DriveArcade.java
+++ 2022RapidReactCode/src/main/java/frc/robot/commands/DriveArcade.java
@@ -54,7 +54,7 @@ public class DriveArcade extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_drive.DriveArcade(-RobotContainer.getInstance().getdriverJoystick().getY(),RobotContainer.getInstance().getdriverJoystick().getZ());
+        m_drive.DriveArcade(-RobotContainer.getInstance().getdriverJoystick().getY(),RobotContainer.getInstance().getdriverJoystick().getX());
     }
 
     // Called once the command ends or is interrupted.
