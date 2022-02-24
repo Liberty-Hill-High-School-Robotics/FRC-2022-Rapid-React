c0832e7b791c39f0b336dd227f11aadc587b90d9
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2022-02-23 19:58:31 -0600

    tested 2/22/22
    Ran test for intake, belts and turret at right speed, and lift
    swing arms need more speed testing

--- 2022RapidReactCode/src/main/java/frc/robot/commands/TurretLeft.java
+++ 2022RapidReactCode/src/main/java/frc/robot/commands/TurretLeft.java
@@ -60,6 +60,7 @@ public class TurretLeft extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_turret.turretStop();
     }
 
     // Returns true when the command should end.
