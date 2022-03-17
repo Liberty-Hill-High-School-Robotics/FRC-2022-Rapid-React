59842509288528b147affcd2ab28565030f3aa2c
Xactlysharp <andersonhpersonal@gmail.com>
2022-03-17 14:59:33 -0500

    3-17
    new camera angle

--- 2022RapidReactCode/src/main/java/frc/robot/RobotContainer.java
+++ 2022RapidReactCode/src/main/java/frc/robot/RobotContainer.java
@@ -66,7 +66,7 @@ private final Joystick driverJoystick = new Joystick(0);
   private final double TARGET_HEIGHT = 104;
   private final double CAMERA_HEIGHT = 37;
   //private final double CAMERA_ANGLE = 35.008;
-  private final double CAMERA_ANGLE = 52.791;
+  private final double CAMERA_ANGLE = 36.40643673;
 
   
   // A chooser for autonomous commands
